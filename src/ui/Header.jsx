5014c70a8367364aed63b89b1b8b3f7b9ecc7369
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-03 16:49:43 -0300

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Marshal</p>
+    </header>
+  );
+};
+
+export default Header;
