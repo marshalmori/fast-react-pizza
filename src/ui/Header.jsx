b69d1a3baf423845f9d4a4c31a5d09b04b3d8f74
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-04 14:55:49 -0300

    Responsive Design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header className="border-b border-stone-300 bg-yellow-500 uppercase px-4 py-3 ">
+    <header className="border-b border-stone-300 bg-yellow-500 uppercase px-4 py-3 sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
